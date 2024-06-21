cd2742e2ddb71e774a16b67f04f5379011fc362b
Mrigank Rai <mrigankrai05@gmail.com>
2024-06-22 02:32:07 +0530

    added sql

--- thoughts.js
+++ thoughts.js
@@ -8,72 +8,60 @@ app.set('view engine','ejs');
 app.set("views",path.join(__dirname,'views'));
 app.use(express.static(path.join(__dirname,'public')));
 const {v4:uuidv4}=require('uuid');
-
+const mysql = require('mysql2');
 app.use(express.urlencoded({extended:true}));
-let posts=[
-    {
-        id:uuidv4(),
-        username:"apna college",
-        content:"learning"
-    },
-    {
-        id:uuidv4(),
-        username:"college",
-        content:"learning in college"
-    },
-    {
-        id:uuidv4(),
-        username:" school ",
-        content:" learning in school "
-    }
-];
+const connection=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'chiragrai',
+    database:'data',
+});
 app.listen(port,()=>{
     console.log('server is running on port'+ port); 
 });
 
-
-
 app.get("/posts",(req,res)=>{
-    res.render("index.ejs",{posts});
+    connection.query(`SELECT * FROM thoughts`,(err,posts)=>{
+        res.render("index.ejs",{posts});
+    })
 });
-
 
 app.get("/posts/create",(req,res)=>{
     res.render("create.ejs");
 });
+
 app.post("/posts",(req,res)=>{
     let username=req.body.username;
     let content=req.body.content;
-    let post={
-        id:uuidv4(),
-        username,
-        content
-    };
-    posts.push(post);
-    res.redirect("/posts");
+    let id=uuidv4()
+    let post=[ id,username,content];
+    connection.query(`INSERT INTO thoughts (id, username, content) VALUES (?,?,?)`,post,(err,result)=>{
+        res.redirect("/posts");
+    });
 });
 
 app.get("/posts/:id/view",(req,res)=>{
     let {id}=req.params;
-    let post=posts.find((p)=>id === p.id);
-    res.render("ok.ejs",{post});
 });
 
 app.delete("/posts/:id", (req, res) => {
     let { id }=req.params;
-    posts = posts.filter((p)=>id!==p.id);
-    res.redirect("/posts");
+    connection.query(`DELETE FROM thoughts where id="${id}"`,(err,result)=>{
+        res.redirect("/posts");
+    });
 });
 
 app.patch("/posts/:id", (req, res) => {
     let { id }=req.params;
-    let post=posts.find((p)=>id === p.id);
-    console.log(req.body);
-    post.content=req.body.content;
-    res.redirect("/posts");
+    let a=req.body.content;
+    connection.query(`UPDATE thoughts SET content="${a}" WHERE id="${id}"`,(err,result)=>{
+        res.redirect("/posts");
+    });
 });
 app.get("/posts/:id", (req, res) => {
     let { id }=req.params;
-    let post=posts.find((p)=>id === p.id);
-    res.render("edit.ejs",{post});
+    connection.query(`SELECT * FROM thoughts where id="${id}"`,(err,result)=>{
+        let post=result[0];
+        res.render("edit.ejs",{post});
+    });
 });
